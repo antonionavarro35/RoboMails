4c232ca9e9fed73b1437f117adfa2d9428bf5ab5
firstcommit <navarro.iglesias747@gmail.com>
2021-03-16 10:39:58 +0100

    Modificaciones varias
    GetMail ahora filtra por Bonificados y crea dos listas. Una de mails que solo llevan el mes anterior de retraso y otra para los que llevan muchos retrasos.
    
    El robot abre ahora el archivo docx con el texto a copiar

--- Scripts/getMails.py
+++ Scripts/getMails.py
@@ -5,22 +5,50 @@ path='C:\\Users\\A.NAVARRO\\Downloads\\'
 imi="IMI_General_21.xlsx"
 newname='Reports_Dump.xlsx'
 pattern='Reports_D*'
+wdir='C:/Users/A.NAVARRO/OneDrive - o2f-it/UiPath/RoboMails/Scripts'
 
-def main():
+def main(month, year):
+
+    mesMasUno=str(int(month)+1)
+
+    month=str(month)
+    year=str(year) 
+    
+    monthYear=month+'/'+year
+    
     rename(path, pattern, newname)
-    df=pd.read_excel(path+imi, sheetName='Main')[['Email', 'ManagerEmail']]
+    df=pd.read_excel(path+imi, sheetName='Main')[['Email', 'ManagerEmail', 'Bonificados']]
+    
     rep=pd.read_excel(path+newname)
-    rep=rep[(rep.Estado=='Pendiente') | (rep.Estado=='Rechazado')]
-    ptes=rep[rep['Fecha del informe']=='1/2021']
+
+    rep[['month','year']] = rep['Fecha del informe'].apply(lambda x: pd.Series(str(x).split("/")))
+    
+    rep=rep[((rep.Estado=='Pendiente') | (rep.Estado=='Rechazado'))&(rep.year==year)]
+
+    countPtes=rep[rep.month!=mesMasUno].groupby('Correo del consultor').agg({'Id':'count'})
+
+    ptes=rep[rep['Fecha del informe']==monthYear]
+    
     new = ptes["Consultor"].str.split(" ", n = 1, expand = True) 
     new=new[[0]].rename(columns={0:'FName'})
     ptes=pd.concat([new,ptes], axis=1)
-    mrg=ptes.merge(df, how='left', left_on='Correo del consultor', right_on='Email')
-    mrg=mrg[['FName','Correo del consultor', 'ManagerEmail', 'Fecha del informe', 'Estado']]
-    data=mrg.rename(columns={'Correo del consultor':'consultor', 'ManagerEmail':'manager', 'Fecha del informe':'mes', 'Estado':'estado'})
-    data.to_csv('C:/Users/A.NAVARRO/OneDrive - o2f-it/UiPath/Mails/Scripts/data/mails.csv', index=False, header=True)
+
+    mrg=ptes.merge(df, how='left', left_on='Correo del consultor', right_on='Email')\
+        [['FName','Correo del consultor', 'ManagerEmail', 'Fecha del informe', 'Estado', 'Bonificados']]
+
+    mrg=mrg[mrg['Bonificados'].isin(['Si', 'si'])]
+
+    mrg=mrg.merge(countPtes, how='left', on='Correo del consultor')
+    
+    mrg=mrg.rename(columns={'Correo del consultor':'consultor', 'ManagerEmail':'manager', 'Fecha del informe':'mes/año', 'Estado':'estado', 'Id': 'backlog'})
+    
+    mrg_soloUno=mrg[mrg.backlog==1]
+    mrg_masdeUno=mrg[mrg.backlog!=1]
+
+    mrg_soloUno.to_csv(wdir+'/data/mails.csv', index=False, header=True)
+    mrg_masdeUno.to_csv(wdir+'/data/mails_nanual.csv', index=False, header=True)
 
 
 if __name__=="__main__":
 
-    main()
+    main(2, 2021)
